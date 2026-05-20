8a1888044337b3f0decc60af25965969d6047e2a
salima <sahisalima77@gmail.com>
2026-05-20 13:38:54 +0200

    déclarer les page login et  register

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,17 +2,24 @@ import { Route, Routes, useLocation } from "react-router"
 import MainLayout from "./UI/layouts/MainLayout"
 import Dashboard from "./UI/pages/Dashboard";
 import AppHeader from "./UI/layouts/AppHeader";
+import Login from "./UI/pages/Login";
+import Register from "./UI/pages/Register";
 
 
 function App() {
   const { pathname } = useLocation();
+  const isAuthPage=pathname === "/login" || pathname === "register"
   
 
   return (
     <>
-   
-
-
+    {isAuthPage ?(
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+    ):(
+      
     <MainLayout >
    
     <Routes>
@@ -21,6 +28,14 @@ function App() {
 
     </Routes>
     </MainLayout >
+
+    )
+
+    }
+
+   
+
+
      
     </>
   )
