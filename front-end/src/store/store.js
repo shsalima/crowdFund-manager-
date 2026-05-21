7990e1e73689296d80953a542b85fa61297179e9
salima <sahisalima77@gmail.com>
2026-05-21 12:04:58 +0200

    ajouter projetsSlice dans le store*

--- front-end/src/store/store.js
+++ front-end/src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./slices/authSlice";
+import projectSlice from "./slices/projectSlice"
 
 
 export const store=configureStore({
     reducer:{
-        auth: authSlice
+        auth: authSlice,
+        projects:projectSlice
     }
 })
